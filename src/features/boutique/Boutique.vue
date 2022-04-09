b6c5e60ef5cd129f046c83146140c3e06c3bdacc
dymafr <contact@dyma.fr>
2022-04-09 14:50:30 +0200

    maj

--- src/features/boutique/Boutique.vue
+++ src/features/boutique/Boutique.vue
@@ -25,79 +25,11 @@ import { useProducts } from './stores/productStore';
 const productStore = useProducts();
 const cartStore = useCart();
 
-// provide(pageKey, toRef(state, 'page'));
-
 // watch([() => state.filters.priceRange, () => state.filters.category], () => {
 //   state.page = 1;
 //   state.products = [];
 // });
 
-// watchEffect(async () => {
-//   state.isLoading = true;
-//   const products = await fetchProducts(state.filters, state.page);
-//   if (Array.isArray(products)) {
-//     state.products = [...state.products, ...products];
-//     if (products.length < 20) {
-//       state.moreResults = false;
-//     }
-//   } else {
-//     state.products = [...state.products, products];
-//   }
-//   state.isLoading = false;
-// });
-
-// function addProductToCart(productId: string): void {
-//   const product = state.products.find((product) => product._id === productId);
-//   if (product) {
-//     const productInCart = state.cart.find(
-//       (product) => product._id === productId
-//     );
-//     if (productInCart) {
-//       productInCart.quantity++;
-//     } else {
-//       state.cart.push({ ...product, quantity: 1 });
-//     }
-//   }
-// }
-
-// function removeProductFromCart(productId: string): void {
-//   const productFromCart = state.cart.find(
-//     (product) => product._id === productId
-//   );
-//   if (productFromCart?.quantity === 1) {
-//     state.cart = state.cart.filter((product) => product._id !== productId);
-//   } else {
-//     productFromCart.quantity--;
-//   }
-// }
-
-// function updateFilter(filterUpdate: FilterUpdate) {
-//   if (filterUpdate.search !== undefined) {
-//     state.filters.search = filterUpdate.search;
-//   } else if (filterUpdate.priceRange) {
-//     state.filters.priceRange = filterUpdate.priceRange;
-//   } else if (filterUpdate.category) {
-//     state.filters.category = filterUpdate.category;
-//   } else {
-//     state.filters = { ...DEFAULT_FILTERS };
-//   }
-// }
-
-// const cartEmpty = computed(() => state.cart.length === 0);
-
-// const filteredProducts = computed(() => {
-//   return state.products.filter((product) => {
-//     if (
-//       product.title
-//         .toLocaleLowerCase()
-//         .startsWith(state.filters.search.toLocaleLowerCase())
-//     ) {
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   });
-// });
 </script>
 
 <template>
